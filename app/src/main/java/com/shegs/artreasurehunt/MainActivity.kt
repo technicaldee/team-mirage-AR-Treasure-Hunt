8932b866c962ac1e4035efa772e73858196cd2ea
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-10-11 10:03:38 +0100

    AR Camera

--- app/src/main/java/com/shegs/artreasurehunt/MainActivity.kt
+++ app/src/main/java/com/shegs/artreasurehunt/MainActivity.kt
@@ -3,16 +3,22 @@ package com.shegs.artreasurehunt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.GoogleMap
 import com.shegs.artreasurehunt.ui.theme.ARTreasureHuntTheme
 import io.github.sceneview.Scene
@@ -56,20 +62,55 @@ fun ARCameraScreen() {
 //    )
 //    arNodes.add(gemstoneNode)
 
-    ARScene(
-        modifier = Modifier.fillMaxSize(),
-        planeRenderer = true,
-        onCreate = { arSceneView ->
-            // Apply AR configuration here
-        },
-        onSessionCreate = { session ->
-            // Configure ARCore session
-        },
-        onFrame = { arFrame ->
-            // Handle AR frame updates
-        },
-        onTap = { hitResult ->
-            // Handle user interactions in AR
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            planeRenderer = true,
+            onCreate = { arSceneView ->
+                // Apply AR configuration here
+            },
+            onSessionCreate = { session ->
+                // Configure ARCore session
+            },
+            onFrame = { arFrame ->
+                // Handle AR frame updates
+            },
+            onTap = { hitResult ->
+                // Handle user interactions in AR
+            }
+        )
+
+        OverlayItems()
+    }
+}
+
+@Composable
+fun OverlayItems() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Add your UI elements here, e.g., buttons and text
+        Button(
+            onClick = { /* Handle button click */ },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Button 1")
         }
-    )
+
+        Button(
+            onClick = { /* Handle button click */ },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Button 2")
+        }
+
+        Text("Text Overlay", modifier = Modifier.padding(8.dp))
+    }
 }
